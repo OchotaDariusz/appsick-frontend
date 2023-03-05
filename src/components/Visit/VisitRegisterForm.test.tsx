18daf43984f6dc3a25b2b0bbe1d67a3e511d2556
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-03-05 14:02:27 +0100

    test: fix for VisitRegisterForm test

--- src/components/Visit/VisitRegisterForm.test.tsx
+++ src/components/Visit/VisitRegisterForm.test.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import { describe, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 
+import { BrowserRouter } from "react-router-dom";
 import VisitRegisterForm from "./VisitRegisterForm";
 
 describe("VisitRegisterForm", () => {
   it("Renders Visit Register Form", () => {
     // ARRANGE
-    render(<VisitRegisterForm />);
+    render(
+      <BrowserRouter>
+        <VisitRegisterForm />
+      </BrowserRouter>
+    );
     // ACT
     // EXPECT
     expect(screen.getByText("What kind of service do you require?:")).toBeVisible();
   });
   it("Renders Visit Register Form buttons", () => {
     // ARRANGE
-    render(<VisitRegisterForm />);
+    render(
+      <BrowserRouter>
+        <VisitRegisterForm />
+      </BrowserRouter>
+    );
     // ACT
     // EXPECT
     expect(screen.getByRole("button")).toHaveTextContent("Register");
